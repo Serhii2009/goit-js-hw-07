a4f2e807eb8bd7d8e56a993a866cc45a1171b96b
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-11-11 21:30:02 +0200

    sms new

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,5 @@
 import { galleryItems } from './gallery-items.js';
-// import 'basiclightbox/dist/basicLightbox.min.css';
-// import * as basicLightbox from 'basiclightbox';
 
-// 1. Створення і рендер розмітки
 const galleryContainer = document.querySelector('.gallery');
 
 const createGalleryItem = ({ preview, original, description }) => {
@@ -16,7 +13,6 @@ const createGalleryItem = ({ preview, original, description }) => {
 const galleryMarkup = galleryItems.map(createGalleryItem).join('');
 galleryContainer.innerHTML = galleryMarkup;
 
-// 2. Реалізація делегування
 galleryContainer.addEventListener('click', event => {
   event.preventDefault();
 
@@ -26,7 +22,6 @@ galleryContainer.addEventListener('click', event => {
 
   const largeImageURL = event.target.closest('.gallery__link').href;
 
-  // 3. Підключення basicLightbox і відкриття модального вікна
   const instance = basicLightbox.create(`
     <img src="${largeImageURL}" width="800" height="600">
   `);
