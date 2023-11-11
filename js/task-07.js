db161c8ce7904b67ec910ba8634b7134fc17551d
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-11-11 16:54:07 +0200

    Some delete

--- js/task-07.js
+++
@@ -1,6 +0,0 @@
-const buttonBall = document.querySelector('#font-size-control');
-const textUp = document.querySelector('#text');
-
-buttonBall.addEventListener('input', () => {
-  text.style.fontSize = `${buttonBall.value}px`;
-});
