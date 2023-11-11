09dfdc69800859c07a2e6be21e4ef4e365ababa5
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-11-11 19:11:06 +0200

    Доданий код js але не повний

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// import 'basiclightbox/dist/basicLightbox.min.css';
+// import * as basicLightbox from 'basiclightbox';
 
-console.log(galleryItems);
+// 1. Створення і рендер розмітки
+const galleryContainer = document.querySelector('.gallery');
+
+const createGalleryItem = ({ preview, original, description }) => {
+  return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+              <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+          </li>`;
+};
+
+const galleryMarkup = galleryItems.map(createGalleryItem).join('');
+galleryContainer.innerHTML = galleryMarkup;
+
+// 2. Реалізація делегування
+galleryContainer.addEventListener('click', event => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const largeImageURL = event.target.closest('.gallery__link').href;
+
+  // 3. Підключення basicLightbox і відкриття модального вікна
+  const instance = basicLightbox.create(`
+    <img src="${largeImageURL}" width="800" height="600">
+  `);
+
+  instance.show();
+});
