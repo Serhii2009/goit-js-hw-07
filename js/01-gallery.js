18ba983ceadb136b7d6f64c94b681eb52a354a74
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-11-12 16:48:13 +0200

    new

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,7 +5,7 @@ const galleryContainer = document.querySelector('.gallery');
 const createGalleryItem = ({ preview, original, description }) => {
   return `<li class="gallery__item">
             <a class="gallery__link" href="${original}">
-              <img class="gallery__image" src="${preview}" alt="${description}" />
+              <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
             </a>
           </li>`;
 };
@@ -13,18 +13,22 @@ const createGalleryItem = ({ preview, original, description }) => {
 const galleryMarkup = galleryItems.map(createGalleryItem).join('');
 galleryContainer.innerHTML = galleryMarkup;
 
-galleryContainer.addEventListener('click', event => {
+galleryContainer.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(event) {
   event.preventDefault();
 
-  if (event.target.nodeName !== 'IMG') {
+  const { nodeName, dataset } = event.target;
+
+  if (nodeName !== 'IMG') {
     return;
   }
 
-  const largeImageURL = event.target.closest('.gallery__link').href;
+  const largeImageURL = dataset.source;
 
   const instance = basicLightbox.create(`
     <img src="${largeImageURL}" width="800" height="600">
   `);
 
   instance.show();
-});
+}
